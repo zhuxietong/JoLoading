3a131bfc294967aa75346a9d2ffc2d42b5e12e17
zhu xietong <zhuxietong@zhudeMacBook-Pro.local>
2017-09-27 15:09:24 +0800

    delete

--- Example/Pods/Target Support Files/Pods-JoLoading_Example/Pods-JoLoading_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JoLoading_Example : NSObject
-@end
-@implementation PodsDummy_Pods_JoLoading_Example
-@end
--- Example/Pods/Target Support Files/Pods-JoLoading_Tests/Pods-JoLoading_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JoLoading_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_JoLoading_Tests
-@end
