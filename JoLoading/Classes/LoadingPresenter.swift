497672aace68857717751cc18f9f5726742e53f9
zhu xietong <zhuxietong@me.com>
2017-12-26 22:04:41 +0800

    support UIView to loading

--- JoLoading/Classes/LoadingPresenter.swift
+++ JoLoading/Classes/LoadingPresenter.swift
@@ -106,3 +106,85 @@ public extension LoadingPresenter where Self:UIViewController
     
 }
 
+public extension LoadingPresenter where Self:UIView
+{
+    
+    public var LoadingPresenterBg:UIView{get{
+        return self
+    }}
+    
+    
+    public var LoadingClass:JoLoading.Type{
+        get{
+            return LoadingPresenterDefautLoadingClass
+        }
+    }
+    
+    public var loadingV:JoLoading{
+        get{
+            
+            if let loadI = self.LoadingPresenterBg.viewWithTag(loadingTag) as? JoLoading
+            {
+                return loadI
+            }
+            createLoadView()
+            let v = self.LoadingPresenterBg.viewWithTag(loadingTag) as! JoLoading
+            return v
+        }
+        set{
+            self.LoadingPresenterBg.viewWithTag(loadingTag)?.removeFromSuperview()
+            let loadV = newValue
+            loadV.tag = loadingTag
+            LoadingPresenterBg.insertSubview(loadV, aboveSubview: jo_contentView)
+            LoadingPresenterBg.eelay = [
+                [loadV,[ee.T.L.B.R]]
+            ]
+            loadV.alpha = 0
+            
+            //            loadV.dismiss(animated: false)
+        }
+        
+    }
+    
+    public var jo_contentView:UIView{
+        get{
+            
+            if let contentV = self.LoadingPresenterBg.viewWithTag(contentTag)
+            {
+                return contentV
+            }
+            createLoadView()
+            return self.LoadingPresenterBg.viewWithTag(contentTag)!
+        }
+    }
+    
+    
+    public func __insert_jo_contentView(_ contentV:UIView){
+        self.LoadingPresenterBg.eelay = [
+            [contentV,[ee.T.L.B.R]],
+        ]
+    }
+    
+    private func createLoadView(){
+        
+        
+        let contentV = UIView()
+        self.LoadingPresenterBg.insertSubview(contentV, at: 0)
+        __insert_jo_contentView(contentV)
+        
+        let loadV = LoadingClass.init()
+        self.LoadingPresenterBg.insertSubview(loadV, aboveSubview: contentV)
+        LoadingPresenterBg.eelay = [
+            [loadV,[ee.T.L.B.R,[0.&900,0.&900,0.&900,0.&900]]]
+        ]
+        
+        loadV.tag = loadingTag
+        contentV.tag = contentTag
+        //        loadV.dismiss(animated: false)
+        loadV.alpha = 0
+    }
+    
+}
+
+
+
